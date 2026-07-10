49b41550176fbb72eea3911121e3cc7747e414a2
AV-Alumno <AV-Alumno@duoc.acad>
2026-07-10 16:40:46 -0400

    Se incorpora la opción 2

--- Rafael_Rivera_Examen.py
+++ Rafael_Rivera_Examen.py
@@ -22,11 +22,11 @@ def leer_opcion():
         
         if op > 6 or op < 1:
             raise ValueError
-        else:
-            return op
     except ValueError:
         print('Debe seleccionar una opción válida')
         return None
+    else:
+        return op
 ########################################
 
 # OP 1 ################################# si es posible agregar deteccion de planes que no existen
@@ -47,6 +47,26 @@ def cupos_tipo(tipo):
     print(f'Existen {totalCupos} cupos en el plan {tipo}')
 ########################################
 
+# OP 2 #################################
+def busqueda_precio(p_min, p_max):
+    planesRango = []
+    
+    if p_min <= p_max and p_min >= 0 and p_max >= 0:
+        for i in inscripciones:
+            if p_min <= inscripciones[i][0] <= p_max:
+                planesRango.append(f'{planes[i][0]}--{i}')
+                #print(planesRango)
+                
+        if planesRango != []:
+            for j in sorted(planesRango):
+                print(j)
+        else:
+            print('No hay planes en ese rango de precios')
+            
+    else:
+        print('El precio máximo no puede ser menor al precio máximo y ambos tienen que ser mayor o igual que cero')
+########################################
+
 while True:
     print('========== MENÚ PRINCIPAL ==========')
     print('1. Cupos por tipo de plan')
@@ -64,3 +84,14 @@ while True:
     elif op == 1:
         tipoPlan = input('Escriba el plan a buscar: ').lower()
         cupos_tipo(tipoPlan)
+    elif op == 2:
+        while True:
+            try:
+                p_min = int(input('Ingrese el precio mínimo: '))
+                p_max = int(input('Ingrese el precio máximo: '))
+            except ValueError:
+                print('Debe ingresar valores enteros')
+                continue
+            else:
+                busqueda_precio(p_min, p_max)
+                break
